a9390f90d84274fffa2538c892d8a347b528d2f9
Rogelio Zamudio <yeyozaz@gmail.com>
2019-01-21 13:03:27 -0600

    added basic tests to BookSearch.test.js to check for shallow render of custom component, current status fails

--- src/components/BookSearch/BookSearch.test.js
+++ src/components/BookSearch/BookSearch.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import BookSearch from './BookSearch';
 
 describe('BookSearch', () => {
-  
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<BookSearch />));
+
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
+  it('should render a <div />', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  });
 })
